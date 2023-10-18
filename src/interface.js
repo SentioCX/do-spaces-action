63dc3367e0c11c7d8894f09151f69b7cdb98c329
Razvan Timis <r@timis.dev>
2023-10-18 12:59:30 +0300

    add debug log

--- src/interface.js
+++ src/interface.js
@@ -1,12 +1,17 @@
 const AWS = require('aws-sdk')
 const fs = require('fs')
 const { lookup } = require('mime-types')
+const core = require('@actions/core')
 
 class S3Interface {
 	constructor(config) {
 		this.bucket = config.bucket
 		this.permission = config.permission
 		this.gzipFileTypes = config.gzipFileTypes
+
+		core.debug(`Creating S3 client for bucket ${ this.bucket }`)
+		core.debug(`Using permission ${ this.permission }`)
+		core.debug(`Using gzip file types ${ this.gzipFileTypes }`)
 
 		const spacesEndpoint = new AWS.Endpoint(`${ config.region }.digitaloceanspaces.com`)
 		const s3 = new AWS.S3({
@@ -35,6 +40,7 @@ class S3Interface {
 			const isMatchingGzipFileType = this.gzipFileTypes.some(
         (fileType) => fileType === contentType
 			)
+			core.debug(`File name = ${ file } with content type = ${ contentType } is matching gzip file type ${ isMatchingGzipFileType }`)
 			if (isMatchingGzipFileType) {
 				options.ContentEncoding = 'gzip'
 			}
